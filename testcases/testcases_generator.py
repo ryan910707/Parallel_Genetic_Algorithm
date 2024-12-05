6cc247e4435463118977addcbfb30b9a4d5b9a13
erictsaii <erictsai90@gmail.com>
2024-12-05 17:09:49 +0800

    Add testcases generator

---
+++ testcases/testcases_generator.py
@@ -0,0 +1,27 @@
+import random
+
+FILENAME = "n10.txt"
+KNAPSACK_CAPACITY = 120
+ITEMS_NUM = 10
+MAX_WEIGHT = 50
+MAX_VALUE = 100
+
+def generate_knapsack_testcase(filename=FILENAME, max_capacity=KNAPSACK_CAPACITY, max_items=ITEMS_NUM, max_weight=MAX_WEIGHT, max_value=MAX_VALUE):
+    capacity = max_capacity
+    num_items = max_items
+
+    items = []
+    for _ in range(num_items):
+        weight = random.randint(10, max_weight)
+        value = random.randint(10, max_value)
+        items.append((weight, value))
+
+    with open(filename, 'w') as f:
+        f.write(f"{capacity}\n")  
+        f.write(f"{num_items}\n")  
+        for weight, value in items:
+            f.write(f"{weight} {value}\n") 
+
+    print(f"Testcase generated and saved to {filename}")
+
+generate_knapsack_testcase()
